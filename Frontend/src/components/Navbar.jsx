0a572b76695cb8f11a953315edebbf01f9fb7524
rushi padaval <rushikeshpadaval@outlook.com>
2025-01-21 18:24:24 +0530

    Changes

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 
 function Navbar() {
+
+    const navItems = (
+        <>
+        
+        <li><a>Home</a>
+      </li>
+     
+      <li><a>Course</a>
+      </li>
+      
+      <li><a>Contact</a>
+      </li>
+      
+      <li><a>About</a>
+      </li>
+
+        </>
+    )
   return (
     <>
       <div>
@@ -24,32 +42,17 @@ function Navbar() {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
+      
+      {navItems}
           </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
+       
+    
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="text-2xl font-bold cursor-pointer">bookStore</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+        {navItems}
     </ul>
   </div>
   <div className="navbar-end">
