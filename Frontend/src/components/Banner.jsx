de3cb1e09a7631c553614b8e39bb322d1ead5b24
Rushikesh Padaval <rushikeshpadaval@outlook.com>
2025-01-25 10:48:20 +0530

    navbar 25% complete

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -3,10 +3,20 @@ import React from 'react';
 const Banner = () => {
   return (
     <>
- <div className="max-w-screen-2x1 container mx-auto md:px-50 px-4 ">
-    <div className='w-1/2' >Left</div>
-    <div className='w-1/2' >Right</div>
-</div>
+ <div className="max-w-screen-2x1 container mx-auto md:px-20 px-4 flex flex-col md:flex-row ">
+    <div className='w-full md:w-1/2' >
+    <div className='space-y-12 mt-12 md:mt-32 '>
+    <h1 className='text-4xl font-bold '>Discover, read, and fall in love with books that inspire. Your perfect story is
+       <span className='text-pink-500'> just a click away!</span>
+       </h1>
+       <p>
+       Dive into a vast collection of books across genres, from thrilling adventures to heartwarming tales. Whether you're a casual reader or a passionate bookworm, we have something for everyone. Explore our carefully curated selection and let every page take you on a new journey.
+
+       </p>
+       </div>
+       </div>
+    <div className='w-full  md:w-1/2' >Right</div>
+    </div>
     </>
   );
 }
