cca89ae72f8b5547f53c602aeb147d389fad1650
Rushikesh Padaval <rushikeshpadaval@outlook.com>
2025-01-27 09:09:45 +0530

    bug fixed

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import list from "../../public/list.json"
+
+const filterData = list.filter ((data)=>data.category=== "free" );
+console.log(filterData);
+ 
 
 function Freebook() {
+
 
     return (
         <div>
