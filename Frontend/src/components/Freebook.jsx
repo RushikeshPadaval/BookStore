1fd880b4bac16a3d7f6c1506145f21ae3f83d4c2
Rushikesh Padaval <rushikeshpadaval@outlook.com>
2025-01-28 18:27:20 +0530

    changes

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import list from "../../public/list.json"
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 import Slider from "react-slick";
 
