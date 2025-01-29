ddcdb554dc1644a037b48543dcd98f32c112976c
Rushikesh Padaval <rushikeshpadaval@outlook.com>
2025-01-29 12:39:53 +0530

    changes

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -12,8 +12,8 @@ function Freebook() {
         dots: true,
         infinite: false,
         speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         initialSlide: 0,
         responsive: [
           {
