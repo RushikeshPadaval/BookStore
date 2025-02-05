c8fae083c0d18b09c2704c7d763775222dfc9329
rushi padaval <rushikeshpadaval@outlook.com>
2025-02-05 17:16:28 +0530

    book added route working

--- src/Book/Book.jsx
+++ src/Book/Book.jsx
@@ -3,7 +3,6 @@ import Navbar from '../components/Navbar'
 import Books from '../components/Books'
 import Footer from '../components/Footer'
 function Book() {
-
   return (
     <>
     <Navbar />
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import list from '../../public/list.json'
+import Cards from "./Cards";
+
 
 function Books() {
     return(
     <>
-    <div>
-        <h1>Books</h1>
-
+    <div className="max-w-screen-2x1 container mx-auto md:px-20 px-4">
+<div className="mt-28 items-center justify-center text-center " >
+    <h1 className="text-2x1  md:text-4xl" >
+        We're delighted to have you <span className="text-pink-500" > Here! </span> :) 
+        </h1>
+        <p className="mt-12" >
+        "Explore our wide collection of books across various genres, from the latest bestsellers to timeless classics. Whether you're a fan of fiction, non-fiction, self-help, or children's books, we have something for everyone. Discover new authors and dive into captivating stories that will expand your knowledge and imagination. Browse, read reviews, and find your next favorite book today. Start your literary journey with us!"
+        </p>
+        <button className=" mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300 " >
+            Back
+        </button>
+   
+</div>
+    <div  className="mt-12 grid grid-cols-1 md:grid-cols-4 " >
+        {
+            list.map((item)=> (
+                < Cards key= {item.id} item={item} />
+            ))
+            
+        }
     </div>
+        </div>
     </>
     )
 }
