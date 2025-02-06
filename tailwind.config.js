c597ca3210bbbb8ef13d992efe47489710c488c2
rushi padaval <rushikeshpadaval@outlook.com>
2025-02-06 16:02:12 +0530

    minor Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,15 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [
-//     require('daisyui'),
-//   ],
-// }
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode:"class",
   theme: {
-    extend: {
-      spacing: {
-        15: '3.75rem', // Adds mt-15 with 3.75rem
-      },
-    },
+    extend: {},
   },
   plugins: [
     require('daisyui'),
   ],
-  daisyui: {
-    themes: ["light", "dark"],  // Enable both light and dark themes
-  },
-};
+}
+
