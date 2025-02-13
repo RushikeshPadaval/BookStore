5efb5f090374680ffe741c2fb3610ea853861603
rushi padaval <rushikeshpadaval@outlook.com>
2025-02-13 18:39:49 +0530

    3.23

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,21 +1,23 @@
 import User from "../model/user.model.js";
+import bcryptjs from "bcryptjs"
 
-export const signup=(req,res)=>{
+export const signup= async (req,res)=>{
 
     try{
         const {fullname,email,password}=req.body;
-        const user=User.findOne({email})
+        const user= await User.findOne({email})
 
         if (user){
             return res.status(400).json({message:"User already exits"})
 
         }
+        const hashPassword = await bcryptjs.hash(password,10)
         const createdUser=new User({
-            fullname,
-            email, 
-            password
+            fullname: fullname,
+            email: email, 
+            password: hashPassword,
         })
-        createdUser.save()
+        await createdUser.save()
         res.status(201).json({message:"User Created Successfully"})
 
 
