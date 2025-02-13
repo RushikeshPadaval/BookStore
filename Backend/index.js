40b0540827d7ab468d3b522e875637223cc83079
rushi padaval <rushikeshpadaval@outlook.com>
2025-02-13 17:45:42 +0530

    minor change

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,9 @@ import express from "express";;
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from 'cors';
+
 import bookRoute from './route/book.route.js'
+import userRoute from './route/user.route.js'
 const app = express()
 
 app.use(cors());
@@ -32,7 +34,8 @@ try{
 
 //defining routes
 
-app.use("/book",bookRoute)
+app.use("/book",bookRoute);
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
