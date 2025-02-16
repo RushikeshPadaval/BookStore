6981fe707e1572505fe2356cd574d6ba1acad558
rushi padaval <rushikeshpadaval@outlook.com>
2025-02-16 19:54:34 +0530

    bug

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,14 +5,18 @@ import Book  from './Book/Book';
 import Signup from './components/Signup';
 import Contact from './components/Contact';
 import  { Toaster } from 'react-hot-toast';
+import { useAuth } from './context/AuthProvider';
 function App() {
+  const [authUser,setAuthUser] =useAuth();
+console.log(authUser);
+
   return (
    <>
    <div className='dark:bg-slate-900 dark:text-white'>
 
 <Routes>
   <Route  path='/' element={<Home/>} />
-  <Route path='/books' element= {<Book/>}/>
+  <Route path='/books' element= { authUser? <Book/>:<Navigate to="/signup"/>}/>
   <Route path='/signup' element={<Signup/>} />
   <Route path='/contact' element = {<Contact/>}/>
 </Routes>
