db3c33a3ae17d0ee073301c39dfbc6149b74d6b1
Rushikesh Padaval <rushikeshpadaval@outlook.com>
2025-03-06 07:47:16 +0530

    Deleted unnecessary files

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()], // Use the React plugin
-  build: {
-    outDir: 'dist', // Output directory for the build files
-  },
-});
